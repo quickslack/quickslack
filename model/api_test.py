ce58c0e4301793ab7f5bf437627bbc5162d88df8
distortedlogic <54854447+Distortedlogic@users.noreply.github.com>
2020-01-20 17:27:00 -0500

    Owens Api Added

---
+++ model/api_test.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+if __name__ == '__main__':
+    url = 'http://127.0.0.1:5000/predict'
+    # url = 'http://model-api:5000/predict'
+    # url = 'http://0.0.0.0:5000/predict'
+    input_text = 'My favorite flavor of ice cream is'
+
+    val = {'input_text': input_text}
+    r_success = requests.post(url, data=json.dumps(val))
+
+    print(f'request responded: {r_success}.')
+    if r_success.status_code == 200:
+        print(f'the content of the response was {r_success.json()}')
+    else:
+        print(r_success)

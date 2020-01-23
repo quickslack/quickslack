dd513bb4c55b1fcecdd26b4100fdc54060d558a9
Richmond Macaspac <richmond@macaspac.com>
2020-01-22 18:45:32 -0600

    notify server about deploy

--- flask/quickslack/sentry.py
+++ flask/quickslack/sentry.py
@@ -3,6 +3,6 @@ from decouple import config
 
 def integrate_sentry(func):
 	sentry_sdk.init(
-		dsn="https://8da7ef327cab437dbeef65a125a66286@sentry.io/1886929", #config('SENTRY_DNS'),
+		dsn=config('SENTRY_DNS'),
 		integrations=[func()]
 	)
